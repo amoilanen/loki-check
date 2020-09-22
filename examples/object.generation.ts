6df4376eb0270cd059ab3809e75531bc5167b072
Anton Moilanen <anton.a.moilanen@gmail.com>
2020-09-22 23:39:58 +0300

    Remove the previous manual generation code

--- examples/object.generation.ts
+++ examples/object.generation.ts
@@ -15,16 +15,6 @@ const names = [ "John", "Martin", "George", "Oliver", "Olivia", "Sophia", "Lily"
 const surnames = [ "Smith", "Jones", "Brown", "Taylor" ];
 const cities = [ "London", "Liverpool", "Bristol", "Manchester", "Bolton" ]
 
-/*
-let name = Generators.oneOfValues(...names).generate();
-let surname = Generators.oneOfValues(...surnames).generate();
-//TODO: A separate integer generating function?
-let age = Generators.choose(0, 120).map(age => Math.floor(age)).generate();
-let city = Generators.oneOfValues(...cities).generate();
-
-let person = new Person(name.get(), surname.get(), age.get(), city.get());
-*/
-
 let person = Generators.object(
   Person,
   Generators.oneOfValues(...names),
