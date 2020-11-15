f1e0651c8317899d38cbd4de4d667eaa00de4b74
Anton Moilanen <anton.a.moilanen@gmail.com>
2020-11-15 21:59:05 +0200

    Move quantifiers to own directory

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as Generators from './generators';
 import { Generator } from './generator';
 import { Maybe } from './maybe';
-import { forAll, exists } from './quantifiers';
 
-export { Generators, Generator, Maybe, forAll, exists };
+export * from './quantifiers';
+export { Generators, Generator, Maybe};
