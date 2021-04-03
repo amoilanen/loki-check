f5fede67bae9fc537b73056c0829816cd9638b32
Anton Moilanen <anton.a.moilanen@gmail.com>
2021-04-03 16:40:47 +0300

    Re-factor duplication in map and flatMap implementations in Generator

--- src/generator.ts
+++ src/generator.ts
@@ -5,29 +5,30 @@ export abstract class Generator<T> {
   abstract generate(): Maybe<T>
 
   map<U>(f: (value: T) => U): Generator<U> {
-    let original = this;
-    return new (class extends Generator<U> {
-      generate() {
-        return original.generate().map(x => f(x));
-      }
-    });
+    return this.flatMap(value => Generator.pure(f(value)));
   }
 
   flatMap<U>(f: (value: T) => Generator<U>): Generator<U> {
-    let original = this;
+    let originalGenerator = this;
     return new (class extends Generator<U> {
       generate() {
-        return original.generate().flatMap(x => f(x).generate());
+        return originalGenerator.generate().flatMap(x => f(x).generate());
       }
     });
   }
 
   static pure<T>(value: T): Generator<T> {
-    let generatedValue = Maybe.pure(value);
-    return new (class extends Generator<T> {
-      generate() {
-        return generatedValue;
-      }
-    })();
+    return new SingleValueGenerator(value);
+  }
+}
+
+class SingleValueGenerator<T> extends Generator<T> {
+  generatedValue: Maybe<T>;
+  constructor(value: T) {
+    super();
+    this.generatedValue = Maybe.pure(value);
+  }
+  generate(): Maybe<T> {
+    return this.generatedValue;
   }
 }
